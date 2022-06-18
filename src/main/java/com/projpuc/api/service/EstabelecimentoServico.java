80c078ee065a248447ef82a875ab51cc9c00497d
Rubens Leme <rubenss.leme@gmail.com>
2022-06-18 03:01:59 -0300

    Criçaão da classe de sestabelecimento servico

--- src/main/java/com/projpuc/api/service/EstabelecimentoServico.java
+++ src/main/java/com/projpuc/api/service/EstabelecimentoServico.java
@@ -2,18 +2,21 @@ package com.projpuc.api.service;
 
 import com.projpuc.api.model.Estabelecimento;
 import com.projpuc.api.repository.EstabelecimentoRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 public class EstabelecimentoServico {
-
+    @Autowired
     private EstabelecimentoRepositorio repositorio;
 
-    public List<Estabelecimento> listaTodosEstabelecimentos(){
+    public List<Estabelecimento> listaTodosEstabelecimentos() {
         return repositorio.findAll();
     }
-    public Estabelecimento inserir(Estabelecimento obj){
+
+    public Estabelecimento inserir(Estabelecimento obj) {
         return repositorio.save(obj);
     }
 
