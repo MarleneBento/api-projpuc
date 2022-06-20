980b618f0ca56e65eecb38544129d0558c05d9f1
Rubens Leme <rubenss.leme@gmail.com>
2022-06-20 01:15:56 -0300

    Implementação do estabelecimento controler e inserção de perfil.

--- src/main/java/com/projpuc/api/controller/EstabelecimentoControlador.java
+++ src/main/java/com/projpuc/api/controller/EstabelecimentoControlador.java
@@ -1,4 +1,24 @@
 package com.projpuc.api.controller;
 
+import com.projpuc.api.model.Estabelecimento;
+import com.projpuc.api.service.EstabelecimentoServico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/estabelecimento")
 public class EstabelecimentoControlador {
+    @Autowired
+    private EstabelecimentoServico estabelecimentoServico;
+
+    @GetMapping
+    public ResponseEntity<List<Estabelecimento>> buscarTodos() {
+        List<Estabelecimento> list = estabelecimentoServico.listaTodosEstabelecimentos();
+        return ResponseEntity.ok().body(list);
+    }
 }
