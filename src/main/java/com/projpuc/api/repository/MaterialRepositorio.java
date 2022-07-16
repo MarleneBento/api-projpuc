3c323790b987dda297fa196c49bf625a733590df
Marlene Bento <marlbento2401@gmail.com>
2022-07-16 13:35:36 -0300

    versao 3

--- src/main/java/com/projpuc/api/repository/MaterialRepositorio.java
+++ src/main/java/com/projpuc/api/repository/MaterialRepositorio.java
@@ -1,7 +1,8 @@
 package com.projpuc.api.repository;
 
+import com.projpuc.api.model.Material;
 import com.projpuc.api.model.Perfil;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MaterialRepositorio extends JpaRepository<Perfil,Long> {
+public interface MaterialRepositorio extends JpaRepository<Material,Long> {
 }
